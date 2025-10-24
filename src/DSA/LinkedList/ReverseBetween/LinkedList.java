31faa3b08e86f34635df196583695e907792289a
shikhak <shikhacodes@gmail.com>
2025-10-24 14:05:19 +0530

    reverse between linked list

---
+++ src/DSA/LinkedList/ReverseBetween/LinkedList.java
@@ -0,0 +1,87 @@
+package DSA.LinkedList.ReverseBetween;
+
+public class LinkedList {
+
+    private Node head;
+    private int length;
+
+    class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public LinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        length = 1;
+    }
+
+    public Node getHead() {
+        return head;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void printList() {
+        Node temp = head;
+        if (temp == null) {
+            System.out.println("empty");
+        } else {
+            while (temp != null) {
+                System.out.print(temp.value);
+                temp = temp.next;
+                if (temp != null) {
+                    System.out.print(" -> ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public void makeEmpty() {
+        head = null;
+        length = 0;
+    }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        length++;
+    }
+
+    public void reverseBetween(int start, int end) {
+        Node dummy = new Node(0);
+        dummy.next = head;
+        Node prev = dummy;
+        for (int i = 0; i < start; i++) {
+            prev = prev.next;
+        }
+        Node current = prev.next;
+
+        for (int i = 0; i < end-start; i++) {
+            Node toMove = current.next;
+            current.next = toMove.next;
+            toMove.next = prev.next;
+            prev.next = toMove;
+        }
+        head = dummy.next;
+    }
+
+
+}
+
+
